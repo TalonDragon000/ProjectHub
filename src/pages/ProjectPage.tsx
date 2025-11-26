24114f492e73bf485620f50e63ba4bad20652b7e
TalonDragon000 <88998252+TalonDragon000@users.noreply.github.com>
2025-11-26 12:15:13 -1000

    Updated ProjectPage.tsx

--- src/pages/ProjectPage.tsx
+++ src/pages/ProjectPage.tsx
@@ -243,7 +243,7 @@ export default function ProjectPage() {
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex justify-between items-center h-16">
             <Link to="/" className="text-xl font-bold text-slate-900">
-              BuildHub
+              ProjectHub
             </Link>
           </div>
         </div>
